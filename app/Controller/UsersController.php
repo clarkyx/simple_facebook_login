1cee802765dfd0abc7956505b448d38580f67146
clark <huajie.chen@utoronto.ca>
2015-04-05 22:02:46 -0400

    fix name

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,7 +1,7 @@
 <?php
 App::uses('AppController','Controller');
 
-class UsersController extends Appcontroller {
+class UsersController extends AppController {
 	public function beforeFilter(){
 		parent::beforeFilter();
 		$this->Auth->allow('add');
